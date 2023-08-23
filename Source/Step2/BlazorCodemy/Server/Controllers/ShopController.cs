f5ba3dbe59a61735641be548a71357f7cca5ba00
alessio iafrate <alessioiafrate@hotmail.com>
2023-08-23 16:10:18 +0200

    show order bar

--- Source/Step2/BlazorCodemy/Server/Controllers/ShopController.cs
+++ Source/Step2/BlazorCodemy/Server/Controllers/ShopController.cs
@@ -20,9 +20,9 @@ namespace BlazorCodemy.Server.Controllers
         public List<ShopElement> Get()
         {
             List<ShopElement> list = new List<ShopElement>();
-            list.Add(new ShopElement() { Name = "Iron man", Description = "desc 1", ImageUrl = "", Price = 150 });
-            list.Add(new ShopElement() { Name = "Thor", Description = "desc 1", ImageUrl = "", Price = 200 });
-            list.Add(new ShopElement() { Name = "Doctor strange", Description = "desc 1", ImageUrl = "", Price =50 });
+            list.Add(new ShopElement() { Name = "Iron man", Description = "desc 1", ImageUrl = "https://i.ebayimg.com/images/g/UTUAAOSwor5kS1YY/s-l500.jpg", Price = 150 });
+            list.Add(new ShopElement() { Name = "Thor", Description = "desc 1", ImageUrl = "https://funkoeurope.com/cdn/shop/products/63732a_Marvel_Thor_POPDiecast_GLAM-WEB_600x.png", Price = 200 });
+            list.Add(new ShopElement() { Name = "Doctor strange", Description = "desc 1", ImageUrl = "https://i.ebayimg.com/images/g/XRIAAOSw~I1iVp1R/s-l500.jpg", Price =50 });
             return list;
             
         }
